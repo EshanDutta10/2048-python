398ea053bda2461cdc2091fe81469ee9c5b726b4
Eshan Dutta <eshandutta06@gmail.com>
2024-06-30 19:22:54 +0530

    defining the tiles and initializing the starting of the game on the window

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ OUTLINE_THICKNESS = 10
 BACKGROUND_COLOR = (205,192,180) #light grey
 FONT_COLOR  = (119,110,101) #dark grey
 
-FONT = pygame.font.SysFont('Fira Code', 60,bold = True)
+FONT = pygame.font.SysFont('consolas', 60,bold = True)
 
 MOVE_VEL = 20 #speed at which the tiles will move when taken action on (pixels/sec)
 
@@ -27,6 +27,58 @@ WINDOW = pygame.display.set_mode((WIDTH,HEIGHT)) # defining the specified sized 
 ICON = pygame.image.load('logo.png') # load the icon image
 pygame.display.set_icon(ICON) # set the window icon
 pygame.display.set_caption("2048 Game") #title 
+
+#defining the tiles (IMPORTANT)
+class Tile:
+    COLORS = [
+        (237,229,218), 
+        (238,225,201), 
+        (243,178,122), 
+        (246,150,101), 
+        (247,124,95), 
+        (247,95,59), 
+        (237,208,115), 
+        (237,204,99), 
+        (236,202,80),
+
+    ]
+
+    def __init__(self,value,row,col):
+        self.value = value
+        self.row = row
+        self.col = col
+        self.x = col * RECT_WIDTH
+        self.y = row * RECT_HEIGHT
+
+    def getColor(self):
+        color_index = int(math.log2(self.value)) - 1 #indexing starts from 0 
+        color = self.COLORS[color_index]
+        return color
+
+    def draw(self,window):
+        #drawing the tile
+        color = self.getColor()
+        pygame.draw.rect(window,color,(self.x,self.y,RECT_WIDTH,RECT_HEIGHT))
+
+        #including the text
+        text = FONT.render(str(self.value),1,FONT_COLOR)
+        window.blit(
+            text,
+            (self.x + (RECT_WIDTH / 2 - text.get_width() / 2),
+             self.y + (RECT_HEIGHT / 2 -text.get_height() / 2 )
+            ),
+        )
+    
+    def setPosition(self):
+        pass
+
+    def move(self, delta):
+        pass
+
+
+
+
+
 
 def drawGrid(window): #drawing the horizontal and vertial line in the window
     for row in range (1,ROWS): 
@@ -41,15 +93,43 @@ def drawGrid(window): #drawing the horizontal and vertial line in the window
 
 
 
-def board(window): #fucntion for board drawing
+def board(window,tiles): #fucntion for board drawing
     window.fill(BACKGROUND_COLOR)
+
+    for tile in tiles.values():
+        tile.draw(window)
+
     drawGrid(window)
     pygame.display.update() #applying changes to screen. it will be updated in order
+
+def getRandomPos(tiles):
+    row = None
+    col = None
+    while True:
+        row = random.randrange(0,ROWS)
+        col = random.randrange(0,COLS)
+
+        if f"{row}{col}" not in tiles:
+            break
+    
+    return row,col
+
+
+
+def generteTiles():
+    tiles = {}
+    for _ in range(2):
+        row,col = getRandomPos(tiles)
+        tiles[f'{row}{col}'] = Tile(2,row,col)
+    return tiles
 
 #Main loop
 def main(window):
     clock = pygame.time.Clock() #regulate the speed of the loop
     run = True
+
+    tiles = generteTiles()
+
 
     while run:
         clock.tick(FPS)
@@ -60,7 +140,7 @@ def main(window):
             if event.type == pygame.QUIT: #to exit the game
                 run = False
                 break
-        board(window)
+        board(window,tiles)
 
     pygame.quit()
 
