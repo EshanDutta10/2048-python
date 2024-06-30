b25f01126c0863a7241cba74dde26b4bd1a79103
Eshan Dutta <eshandutta06@gmail.com>
2024-06-30 18:16:20 +0530

    drawing grids in the main window

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ COLS = 4
 RECT_HEIGHT = HEIGHT//ROWS
 RECT_WIDTH  = HEIGHT// COLS
 
-OOUTLINE_COLOR = (187,174,160) #grey
+OUTLINE_COLOR = (187,174,160) #grey
 OUTLINE_THICKNESS = 10
 BACKGROUND_COLOR = (205,192,180) #light grey
 FONT_COLOR  = (119,110,101) #dark grey
@@ -27,6 +27,24 @@ WINDOW = pygame.display.set_mode((WIDTH,HEIGHT)) # defining the specified sized 
 ICON = pygame.image.load('logo.png') # load the icon image
 pygame.display.set_icon(ICON) # set the window icon
 pygame.display.set_caption("2048 Game") #title 
+
+def drawGrid(window): #drawing the horizontal and vertial line in the window
+    for row in range (1,ROWS): 
+        y = row * RECT_HEIGHT
+        pygame.draw.line(window,OUTLINE_COLOR,(0,y),(WIDTH,y),OUTLINE_THICKNESS)
+    
+    for column in range(1,COLS):    
+        x = column * RECT_WIDTH
+        pygame.draw.line(window,OUTLINE_COLOR,(x,0),(x,HEIGHT),OUTLINE_THICKNESS)
+
+    pygame.draw.rect(window,OUTLINE_COLOR,(0,0,WIDTH,HEIGHT), OUTLINE_THICKNESS)
+
+
+
+def board(window): #fucntion for board drawing
+    window.fill(BACKGROUND_COLOR)
+    drawGrid(window)
+    pygame.display.update() #applying changes to screen. it will be updated in order
 
 #Main loop
 def main(window):
@@ -42,7 +60,8 @@ def main(window):
             if event.type == pygame.QUIT: #to exit the game
                 run = False
                 break
-            
+        board(window)
+
     pygame.quit()
 
 if __name__ == "__main__":
